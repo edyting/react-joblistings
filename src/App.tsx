952207cef48bf10f3e21f6541ed8999ce4c601ba
edyting <dankyiemmanuel0@gmail.com>
2024-12-25 21:24:36 +0000

    setting up react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import {createBrowserRouter,createRoutesFromElements,Route,RouterProvider} from "react-router-dom"
 import Hero from "./components/Hero"
 import HomeCards from "./components/HomeCards";
 import JobListings from "./components/JobListings";
 import Navbar from "./components/Navbar"
+import ViewAllJobs from "./components/ViewAllJobs";
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route index element={ } />,
+  )
+);
 
 
 const App = () => {
@@ -20,14 +29,7 @@ const App = () => {
       <JobListings />
       
       {/* <!-- View All Jobs --> */}
-      <section className="m-auto max-w-lg my-10 px-6">
-        <a
-          href="jobs.html"
-          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >
-          View All Jobs
-        </a>
-      </section>
+     <ViewAllJobs/>
     </div>
   );
 }
---
+++ src/components/JobListing.tsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import {FaMapMarker } from "react-icons/fa";
+
+interface Props {
+  job: {
+    id: string;
+    title: string;
+    type: string;
+    description: string;
+    location: string;
+    salary: string;
+    company: {
+      name: string;
+      description: string;
+      contactEmail: string;
+      contactPhone: string;
+    };
+  };
+
+}
+
+
+
+const JobListing = ({ job }: Props) => {
+    const [showmore, setShowmore] = useState(false);
+
+    let description = job.description;
+
+    if (!showmore) {
+        description = description.substring(0, 100);
+     }
+
+  return (
+    <div>
+      <div className="bg-white rounded-xl shadow-md relative">
+        <div className="p-4">
+          <div className="mb-6">
+            <div className="text-gray-600 my-2">{job.type}</div>
+            <h3 className="text-xl font-bold">{job.title} </h3>
+          </div>
+
+                  <div className="mb-5">{description} <span>{!showmore && "..." }</span></div>
+                  <button onClick={() => {
+                      setShowmore((prevState)=>!prevState);
+                  }} className="text-indigo-500 hover:text-indigo-600 mb-5">{!showmore ? "More":"Less" }</button>
+
+          <h3 className="text-indigo-500 mb-2">{job.salary + " / Year"}</h3>
+
+          <div className="border border-gray-100 mb-5"></div>
+
+          <div className="flex flex-col lg:flex-row justify-between mb-4">
+            <div className="text-orange-700 mb-3">
+                <FaMapMarker className="inline text-sm mr-1 mb-1" />
+              {job.location}
+            </div>
+            <a
+              href={`/job/${job.id}`}
+              className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
+            >
+              Read More
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default JobListing
