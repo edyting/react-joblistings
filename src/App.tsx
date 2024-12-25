e1ccd502fc2699ea9eb899328f0681581cafc012
edyting <dankyiemmanuel0@gmail.com>
2024-12-25 21:41:39 +0000

    added jobs page and updated app.tsx with the new route

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import {createBrowserRouter,createRoutesFromElements,Route,RouterProvider} from "react-router-dom"
 import Home from "./pages/Home";
 import MainLayout from "./layout/MainLayout";
+import JobsPage from "./pages/JobsPage";
 
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout/>}>
       <Route index element={<Home />} />
+      <Route path="jobs" element={<JobsPage/>} />
     </Route>
   )
 );
---
+++ src/pages/JobsPage.tsx
@@ -0,0 +1,11 @@
+
+const JobsPage = () => {
+  return (
+    <div>
+          <h2>Jobs Page</h2>
+          
+    </div>
+  )
+}
+
+export default JobsPage
